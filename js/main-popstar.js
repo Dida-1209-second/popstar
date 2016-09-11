66a9eeba5104e64e4d081e36b829ea57b9f14d78
Dida-1209-second <duhuiying1209@outlook.com>
2016-09-11 23:13:21 +0800

    init*2

--- js/main-popstar.js
+++ js/main-popstar.js
@@ -1,2 +1,20 @@
 var WIDTH=500;
 var HEIGHT=500;
+
+window.onload=function(){
+	var canvas=document.getElementById("canvas");
+	var context=canvas.getContext('2d');
+	canvas.width=WIDTH;
+	canvas.height=HEIGHT;
+	
+	init();
+}
+
+function init(){
+	for(var i=0;i<10;i++){
+		for(var j=0;j<10;j++){
+			starObj.init(i,j);
+		}
+	}
+	
+}
