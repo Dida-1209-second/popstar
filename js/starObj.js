df14378721708b514e4bb00c70ff14f731085dde
Dida-1209-second <duhuiying1209@outlook.com>
2016-09-14 21:52:59 +0800

    init*2.3

--- js/starObj.js
+++ js/starObj.js
@@ -9,19 +9,23 @@ var starObj=function(){
 }
 
 starObj.prototype.init=function(){
-	for(var n=0;n<100;n++){
-		for(var i=0;i<10;i++){
-			for(var j=0;j<10;j++){
-				this.x[n]=i*50;
-				this.y[n]=j*50;
-				this.color[n]=colors[Math.floor(Math.random()*colors.length)];
-			}
+	var n=0;
+	for(var i=0;i<10;i++){
+		for(var j=0;j<10;j++){
+			this.x[n]=i*50;
+//			console.log(this.x);
+			this.y[n]=j*50;
+			//console.log(this.y);
+			this.color[n]=colors[Math.floor(Math.random()*colors.length)];
+			//console.log(this.color);
+			n++;
 		}
 	}
 }
 starObj.prototype.draw=function(cxt){
 	for(var i=0;i<100;i++){
+		//console.log(this.color[i],this.x[i],this.y[i]);
 		cxt.fillStyle=this.color[i];
-		cxt.roundRect(this.x,this.y,40,40,5).fill();
+		cxt.roundRect(this.x[i],this.y[i],48,48,10).fill();
 	}
 }
