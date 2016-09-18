30f846485130c85d2046e7bcfb75e63b541e0475
Dida-1209-second <duhuiying1209@outlook.com>
2016-09-18 23:35:30 +0800

    game*1

--- js/main-popstar.js
+++ js/main-popstar.js
@@ -1,6 +1,8 @@
 var WIDTH=500;
 var HEIGHT=500;
 var isgameover=false;
+var mx;
+var my;
 
 window.onload=function(){
 	var canvas=document.getElementById("canvas");
@@ -8,14 +10,18 @@ window.onload=function(){
 	canvas.width=WIDTH;
 	canvas.height=HEIGHT;
 	
+	canvas.addEventListener('click',onMouseClick,false);
 	init(context);
 }
 
 function init(cxt){
+	//画星星和圆角矩形
 	star=new starObj();
 	star.init();
 	star.draw(cxt);
 	star.drawStar(cxt);
+	//鼠标点击事件
+	
 }
 
 function newgame(){
@@ -31,7 +37,19 @@ function gameover(){
 }
 
 function Collision(){
+	var arrayi=[];
+	var arrayj=[];
+	if(!!mx%50){
+		arrayi.push(Math.floor(mx/50));
+	}
+	//for(var )
+}
+
+function onMouseClick(e){
 	if(!gameover()){
-		
+		if(e.offsetX||e.layerX){
+			mx=e.offsetX==undefined?e.layerX : e.offsetX;
+			my=e.offsetY==undefined?e.layerY : e.offsetY;
+		}
 	}
 }
--- js/starObj.js
+++ js/starObj.js
@@ -1,6 +1,7 @@
 var starObj=function(){
 	this.color=[];
-	this.depth=[];
+	this.i=[];
+	this.j=[];
 	this.x=[];
 	this.y=[];
 	this.a=[];
@@ -12,6 +13,8 @@ starObj.prototype.init=function(){
 	var n=0;
 	for(var i=0;i<10;i++){
 		for(var j=0;j<10;j++){
+			this.i[n]=i;
+			this.j[n]=j;
 			this.x[n]=i*50;
 			//console.log(this.x);
 			this.y[n]=j*50;
@@ -31,7 +34,7 @@ starObj.prototype.init=function(){
 }
 starObj.prototype.draw=function(cxt){
 	for(var i=0;i<100;i++){
-		console.log(this.color[i],this.x[i],this.y[i]);
+		//console.log(this.color[i],this.x[i],this.y[i]);
 		cxt.fillStyle=this.color[i];
 		cxt.roundRect(this.x[i],this.y[i],48,48,10).fill();
 		
