e55acae3d2de4d84ac169c8229f25d581d39e1db
Dida-1209-second <duhuiying1209@Outlook.com>
2016-09-26 19:01:08 +0800

    修改星星对象以及检测数组

--- js/main-popstar.js
+++ js/main-popstar.js
@@ -1,142 +1,155 @@
-var WIDTH=500;
-var HEIGHT=500;
-var isgameover=false;
+var WIDTH = 500;
+var HEIGHT = 500;
+var isgameover = false;
 var mx;
 var my;
 var star;
-var arr;
-var arrX;
+var arr;//存储点击位置上下左右颜色相同的星星
 
-window.onload=function(){
-	var canvas=document.getElementById("canvas");
-	var context=canvas.getContext('2d');
-	canvas.width=WIDTH;
-	canvas.height=HEIGHT;
-	
-	canvas.addEventListener('click',onMouseClick,false);
+window.onload = function() {
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext('2d');
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
+
+	canvas.addEventListener('click', onMouseClick, false);
 	init(context);
 }
 
-function init(cxt){
+function init(cxt) {
 	//画星星和圆角矩形
-	star=new starObj();
-	
-	star.init();
-	star.draw(cxt);
-	star.drawStar(cxt);
-	//鼠标点击事件
-	
-}
-
-function newgame(){
-	
-}
-
-function gameover(){
-	if(isgameover){
-		alert("gameover");
+	console.log(data.length);
+	for(var i = 0; i < data.length; i++) {
+		for(var j = 0; j < data[i].length; j++) {
+			star = new starObj();
+			star.init(i, j);
+			star.draw(cxt);
+			star.drawStar(cxt);
+			data[i][j] = star;
+		}
 	}
-	else 
-	return false;
+
+	//鼠标点击事件
+
 }
 
-function Collision(i,j){
-	arr=[];
-	arrX=[];
-	arr.push({i:Math.floor(i/50),j:Math.floor(j/50)});
-	var i=0;
+function newgame() {
+
+}
+
+function gameover() {
+	if(isgameover) {
+		alert("gameover");
+	} else
+		return false;
+}
+
+function Collision(i, j) {
+	arr = [];
+	arr.push({
+		i: Math.floor(i / 50),
+		j: Math.floor(j / 50)
+	});
+	var i = 0;
 	//Arr.push({i:Math.floor(i/50),j:Math.floor(j/50)});
-		do{
-			var obj=arr[i];
-			console.log(arr.length);
-			x=obj.i;
-			y=obj.j;
-			console.log(x,y);
-			arrX=checkLeft(x,y);
-			arr=checkDouble(arr,arrX);
-//			debugger
-			arrX=checkRight(x,y);
-			arr=checkDouble(arr,arrX);
-//			debugger
-			arrX=checkUp(x,y);
-			arr=checkDouble(arr,arrX);
-//			debugger
-			arrX=checkDown(x,y);
-			arr=checkDouble(arr,arrX);
-//			debugger
-//			Arr=checkDouble(Arr,arr);
-			for(var j=0;j<arr.length;j++)
-			console.log(arr[j]);
-			i++;
-			
-		}while(i<arr.length);
-		//Horizontal(arrayi,arrayj);
+	do {
+		var obj = arr[i];
+		//			console.log(arr.length);
+		x = obj.i;
+		y = obj.j;
+		//			console.log(x,y);
+		arr = checkDouble(arr, checkLeft(x, y));
+		//			debugger
+		arr = checkDouble(arr, checkRight(x, y));
+		//			debugger
+		arr = checkDouble(arr, checkUp(x, y));
+		//			debugger
+		arr = checkDouble(arr, checkDown(x, y));
+		//					debugger
+		//					for(var j=0;j<arr.length;j++)
+		//					console.log(arr[j]);
+		i++;
+
+	} while (i < arr.length);
+	//Horizontal(arrayi,arrayj);
 
 }
 
-function checkDouble(arr,arrX){
-	for(var i=0;i<arrX.length;i++){
-		var flag=false;
-		var x=arrX[i].i;
-		var y=arrX[i].j;
-		for(var j=0;j<arr.length;j++){
-			if(arr[j].i==x&&arr[j].j==y){
-				flag=true;
+function checkDouble(arr, arrX) {
+	for(var i = 0; i < arrX.length; i++) {
+		var flag = false;
+		var x = arrX[i].i;
+		var y = arrX[i].j;
+		for(var j = 0; j < arr.length; j++) {
+			if(arr[j].i == x && arr[j].j == y) {
+				flag = true;
 			}
 		}
-		if(!flag){
+		if(!flag) {
 			arr.push(arrX[i]);
 		}
 	}
-	return arr; 
+	return arr;
 }
 
-function checkLeft(x,y){
-	var arrL=[];
-	for(var n=x-1;n>=0;n--){
-		//if(data[n][j].color==data[i][j].color){
-			arrL.push({i:n,j:y});
-		//}else break;
+function checkLeft(x, y) {
+	var arrL = [];
+	for(var n = x - 1; n >= 0; n--) {
+		if(data[n][y].color == data[x][y].color) {
+			arrL.push({
+				i: n,
+				j: y
+			});
+		} else break;
 	}
 	return arrL;
 }
 
-function checkRight(x,y){
-	var arrR=[];
-	for(var n=x+1;n<10;n++){
-		//if(data[n][j].color==data[i][j].color){
-			arrR.push({i:n,j:y});
-		//}else break;
+function checkRight(x, y) {
+	var arrR = [];
+	for(var n = x + 1; n < 10; n++) {
+		if(data[n][y].color == data[x][y].color) {
+			arrR.push({
+				i: n,
+				j: y
+			});
+		} else break;
 	}
 	return arrR;
 }
 
-function checkUp(x,y){
-	var arrU=[];
-	for(var n=y-1;n>=0;n--){
-		//if(data[n][j].color==data[i][j].color){
-			arrU.push({i:x,j:n});
-		//}else break;
+function checkUp(x, y) {
+	var arrU = [];
+	for(var n = y - 1; n >= 0; n--) {
+		if(data[x][n].color == data[x][y].color) {
+			arrU.push({
+				i: x,
+				j: n
+			});
+		} else break;
 	}
 	return arrU;
 }
 
-function checkDown(x,y){
-	var arrD=[];
-	for(var n=y+1;n<10;n++){
-		//if(data[n][j].color==data[i][j].color){
-			arrD.push({i:x,j:n});
-		//}else break;
+function checkDown(x, y) {
+	var arrD = [];
+	for(var n = y + 1; n < 10; n++) {
+		if(data[x][n].color == data[x][y].color) {
+			arrD.push({
+				i: x,
+				j: n
+			});
+		} else break;
 	}
 	return arrD;
 }
 
-function onMouseClick(e){
-	if(!gameover()){
-		if(e.offsetX||e.layerX){
-			mx=e.offsetX==undefined?e.layerX : e.offsetX;
-			my=e.offsetY==undefined?e.layerY : e.offsetY;
-			Collision(mx,my);
+function onMouseClick(e) {
+	if(!gameover()) {
+		if(e.offsetX || e.layerX) {
+			mx = e.offsetX == undefined ? e.layerX : e.offsetX;
+			my = e.offsetY == undefined ? e.layerY : e.offsetY;
+			Collision(mx, my);
 		}
 	}
 }
--- js/starObj.js
+++ js/starObj.js
@@ -1,49 +1,53 @@
-var starObj=function(){
-	this.color=[];
-	this.i=[];
-	this.j=[];
-	this.x=[];
-	this.y=[];
-	this.a=[];
-	this.vx=[]
-	this.vy=[];
+var starObj = function() {
+	this.color ;
+	this.i;
+	this.j;
+	this.x ;
+	this.y ;
+	this.a ;
+	this.vx ;
+	this.vy ;
 }
 
-starObj.prototype.init=function(){
-	var n=0;
-	for(var i=0;i<10;i++){
-		for(var j=0;j<10;j++){
-			this.i[n]=i;
-			this.j[n]=j;
-			this.x[n]=i*50;
-			//console.log(this.x);
-			this.y[n]=j*50;
-			//console.log(this.y);
-			this.color[n]=colors[Math.floor(Math.random()*colors.length)];
-			//console.log(this.color);
-			n++;
-		}
-	}
-	
+starObj.prototype.init = function(i,j) {
+	this.i= i;
+	this.j = j;
+	this.x= i * 50;
+	//console.log(this.x);
+	this.y = j * 50;
+	//console.log(this.y);
+	this.color = colors[Math.floor(Math.random() * colors.length)];
+	//console.log(this.color);
+}
+
 //生成背景圆角矩形 
 //	for(var i=0;i<10;i++){
 //		for(j=0;j<10;j++){
 //			
 //		}
 //	}
+
+//圆角矩形
+starObj.prototype.draw = function(cxt) {
+	//console.log(this.color[i],this.x[i],this.y[i]);
+	cxt.fillStyle = this.color;
+	cxt.roundRect(this.x, this.y, 48, 48, 10).fill();
 }
-starObj.prototype.draw=function(cxt){
-	for(var i=0;i<100;i++){
-		//console.log(this.color[i],this.x[i],this.y[i]);
-		cxt.fillStyle=this.color[i];
-		cxt.roundRect(this.x[i],this.y[i],48,48,10).fill();
-		
-	}
+
+starObj.prototype.drawStar = function(cxt) {
+	cxt.fillStyle = this.color;
+	cxt.starFill(20, 10, this.x + 24, this.y + 24).fill();
+	cxt.starStroke(20,10,this.x+24,this.y+24).stroke();
 }
-starObj.prototype.drawStar=function(cxt){
-	for(var i=0;i<100;i++){
-		cxt.fillStyle=this.color[i];
-		cxt.starFill(20,10,this.x[i]+24,this.y[i]+24).fill();
-		cxt.starStroke(20,10,this.x[i]+24,this.y[i]+24).stroke();
-	}
-}
+
+
+
+
+
+
+
+
+
+
+
+
